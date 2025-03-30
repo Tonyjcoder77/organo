ec5b3a220dccb94175d2fa9d5db98476ff0439a1
Tonyjcoder77 <tonyjcoder@gmail.com>
2025-03-30 11:14:27 -0300

    Clear project

--- organo/src/componentes/Banner/Banner.js
+++ organo/src/componentes/Banner/Banner.js
@@ -3,7 +3,8 @@ import './Banner.css'
 function Banner() {
     // JSX
     return (
-        <header className="banner">
+        <header className="banner
+        ">
              <img src="/imagens/banner.png" alt="O banner principal"/>
         </header>
     )
